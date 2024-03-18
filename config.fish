95e73293460ab09a949224203b2a6225b03454d6
Sean Ferguson <seanferguson8148@gmail.com>
2024-03-18 10:17:37 -0400

    removed greeting, changed starship config location

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
+    set fish_greeting
     source $HOME/.config/fish/current-theme.fish
     zoxide init --cmd j fish | source
+    set -gx STARSHIP_CONFIG $HOME/.config/starship/starship-fish.toml
     starship init fish | source
     direnv hook fish | source
 end
