b251fea1b15e9987afc643a79431670ea342cf0b
Sean Ferguson <seanferguson8148@gmail.com>
2024-03-20 15:31:11 -0400

    <C-o> to open ranger-cd

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     set fish_greeting
     source $HOME/.config/fish/current-theme.fish
+    bind \co 'ranger-cd'
     zoxide init --cmd j fish | source
     set -gx STARSHIP_CONFIG $HOME/.config/starship/starship-fish.toml
     starship init fish | source
